3665d93b17d454cbf350b09a944060d0115bcfb6
YuliiaMatich <yuliia.matich@gmail.com>
2023-05-04 22:54:24 -0400

    fix for iterable array

--- uniqueOrder.js
+++ uniqueOrder.js
@@ -3,20 +3,36 @@
 const uniqueInOrder = function (iterable) {
   let result = [];
 
-  let array = iterable.split("");
+  if (typeof iterable === "string") {
+    let array = iterable.split("");
 
-  if (array.length === 0) {
-    return [];
-  }
-  
-  result.push(array[0]);
-  for (let i = 0; i < array.length - 1; i++) {
-    if (array[i] !== array[i + 1]) {
-      result.push(array[i + 1]);
+    if (array.length === 0) {
+      return [];
     }
+
+    result.push(array[0]);
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] !== array[i + 1]) {
+        result.push(array[i + 1]);
+      }
+    }
+    return result;
   }
-  return result;
+
+  if (typeof iterable === "object") {
+    if (iterable.length === 0) {
+      return [];
+    }
+
+    result.push(iterable[0]);
+    for (let i = 0; i < iterable.length - 1; i++) {
+      if (iterable[i] !== iterable[i + 1]) {
+        result.push(iterable[i + 1]);
+      }
+    }
+    return result;
+  }
 };
 
 console.log(uniqueInOrder("AAAABBBCCDAABBB"));
-
+console.log(uniqueInOrder([1, 2, 3, 3, 3]));
